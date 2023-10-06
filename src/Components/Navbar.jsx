fa6be6eef4df885a3495c4d647f76f08dbe3e3ec
FAHIM-33 <shahriyerfahim2012@gmail.com>
2023-10-07 00:00:40 +0600

    Navbar & routes added

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,24 @@
+import { NavLink } from "react-router-dom";
+
+
+const Navbar = () => {
+    let links = <>
+        <li><NavLink to="/" className="p-2 rounded-md block">HOME</NavLink></li>
+        <li><NavLink to="/2" className="p-2 rounded-md block">TWO</NavLink></li>
+        <li><NavLink to="/3" className="p-2 rounded-md block">Three</NavLink></li>
+    </>
+    return (
+        <div>
+            <nav className="grid grid-cols-3 bg-[#111] m-1 rounded-md px-2 py-1">
+                <div></div>
+                <ul className="font-semibold flex justify-center gap-6">
+                    {links}
+                </ul>
+                <div className=" "></div>
+
+            </nav>
+        </div>
+    );
+};
+
+export default Navbar;
