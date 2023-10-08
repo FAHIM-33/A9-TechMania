97e98815e41b88409ae872d1693bc6bff474a7e8
FAHIM-33 <shahriyerfahim2012@gmail.com>
2023-10-08 22:42:48 +0600

    Added AboutUs

--- src/Pages/Membership.jsx
+++ src/Pages/Membership.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import Navbar from "../Components/Navbar";
 import axios from "axios";
 import MemberCard from "../Components/Comments/MemberCard";
+import Footer from "../Components/Footer";
 
 
 const Membership = () => {
@@ -18,7 +19,7 @@ const Membership = () => {
                 <Navbar></Navbar>
             </div>
             <h2 className="text-4xl text-center mb-12 mt-20 pr-4">Purchase our premium membership or get started for free</h2>
-            <section className="grid grid-cols-3 gap-8 p-4">
+            <section className="grid grid-cols-3 gap-8 p-4 mb-20">
                 {
                     data.map(obj => <MemberCard
                         key={obj.id}
@@ -26,6 +27,7 @@ const Membership = () => {
                     ></MemberCard>)
                 }
             </section>
+            <Footer></Footer>
         </div>
     );
 };
